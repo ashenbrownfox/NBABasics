f75da57aaf31fc892acb66d145d0a6d5e68ccd78
ashenbrownfox <newhoopsdynasty@gmail.com>
2021-04-25 13:13:48 -0400

    GSW

--- basics.py
+++ basics.py
@@ -9,8 +9,20 @@ starting_lineup_bulls = [
     {"position": "Guard", "jersey": 23,
         "college":  "North Carolina", "name": "Michael Jordan"},
 ]
+starting_lineup_warriors = [
+    {"position": "Guard", "jersey": 9,
+        "college": "Miami Unviseristy", "name": "Andre Igoudala"},
+    {"position": "Forward", "jersey": 7,
+        "college": "Michigan State", "name": "Draymond Green"},
+    {"position": "Guard", "jersey": 11,
+        "college": "Washington St", "name": "Klay Thompson"},
+    {"position": "Forward", "jersey": 35,
+        "college": "Texas", "name": "Kevin Durant"},
+    {"position": "Guard", "jersey": 30,
+        "college":  "Davidson", "name": "Stephen Curry"},
+]
 
-for player in starting_lineup_bulls:
+for player in starting_lineup_warriors:
     position = player["position"]
     jersey = player["jersey"]
     college = player["college"]
